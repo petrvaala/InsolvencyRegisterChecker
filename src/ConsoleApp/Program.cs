c75b78ca0575dcc54128f47e8a1ee34325fb8f7d
Vala, Petr <petr.vala@rollingbrothers.eu>
2020-05-15 13:15:48 +0200

    add service project and console project for testing purposes

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using InsolvencyRegisterCuzkServiceReference;
+using PetrVala.InsolvencyRegisterChecker.ServiceLibrary;
+
+namespace PetrVala.InsolvencyRegisterChecker.ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var wrapper = new ServiceFullWrapper();
+
+            //var request = new getIsirWsCuzkDataRequest {ic = "64651975", maxRelevanceVysledku = 2};
+            var request = new getIsirWsCuzkDataRequest {ic = "27604977", maxRelevanceVysledku = 2};
+            var resultTask = wrapper.GetIsirWsCuzkDataAsync(request);
+
+            resultTask.Wait();
+
+            var result = resultTask.Result;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ServiceLibrary/ServiceFullWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using InsolvencyRegisterCuzkServiceReference;
+
+namespace PetrVala.InsolvencyRegisterChecker.ServiceLibrary
+{
+    public class ServiceFullWrapper
+    {
+        public async Task<getIsirWsCuzkDataResponse> GetIsirWsCuzkDataAsync(getIsirWsCuzkDataRequest request)
+        {
+            var client = new IsirWsCuzkPortTypeClient();
+            return await client.getIsirWsCuzkDataAsync(request);
+        }
+    }
+}
